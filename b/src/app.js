1e1803f9f218ec6da5e45dfd8aa40ce8d9b6a50d
Solarpaletten <solarpaletten@gmail.com>
2025-03-09 13:53:33 +0200

    fix: separate URLs in CORS origin into individual array elements

--- b/src/app.js
+++ b/src/app.js
@@ -13,7 +13,13 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN
       ? process.env.CORS_ORIGIN.split(',')
-      : ['http://localhost:5173','http://localhost:4000, https://npmfr-snpq.onrender.com',],
+      : [
+          'http://localhost:5173',
+          'http://localhost:3000',
+          'https://npmfr-snpq.onrender.com',
+          'http://localhost:4000',
+          'https://npmbk-ppnp.onrender.com',
+        ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
