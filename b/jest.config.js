8dba06bd3736c787496d89193e4498b5b7b7ad99
Leonid <leonid@solar.com>
2025-04-14 23:32:27 +0000

    test: prevent server startup during tests

--- b/jest.config.js
+++ b/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   testEnvironment: 'node', // Среда выполнения тестов (Node.js)
   setupFilesAfterEnv: ['./tests/setup.js'], // Указать путь до setup-файла
   testMatch: ['**/tests/**/*.test.js'], // Паттерн поиска тестов
+  // Не запускать реальный сервер
+  clearMocks: true,
+  restoreMocks: true,
+  resetMocks: true,
 };
